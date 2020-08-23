318a894276bca85e32fc4d6f52c883e8e8acf57d
madhumithaa <madhumithaa1411@gmail.com>
2020-08-23 19:07:52 +0530

    btn done

--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -3,8 +3,11 @@ import "./Css/Btn.css";
 export default class Btn extends Component {
   render() {
     return (
-      <button type="button" className="btn btn-primary btn-lg btn-block">
-        Block level button
+      <button
+        type="button"
+        className="btn btn-container btn-lg btn-block text-white text-uppercase"
+      >
+        Create your Account
       </button>
     );
   }
