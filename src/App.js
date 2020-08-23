76f5a6d5feaadb869c74a9cab2b5bdc30e8f25ef
madhumithaa <madhumithaa1411@gmail.com>
2020-08-23 19:54:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch, Navigation } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LeftContainer from "./MajorComponents/LeftContainer";
 import Login from "./MajorComponents/Login";
 import Signup from "./MajorComponents/Signup";
@@ -7,12 +7,12 @@ import Signup from "./MajorComponents/Signup";
 export default class App extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-lg-5">
-          <LeftContainer />
-        </div>
-        <div className="col-lg-7">
-          <BrowserRouter>
+      <BrowserRouter>
+        <div className="row">
+          <div className="col-lg-5">
+            <LeftContainer />
+          </div>
+          <div className="col-lg-7">
             <div>
               <Switch>
                 <Route path="/login" component={Login} exact />
@@ -20,9 +20,9 @@ export default class App extends Component {
                 <Route component={Error} />
               </Switch>
             </div>
-          </BrowserRouter>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
