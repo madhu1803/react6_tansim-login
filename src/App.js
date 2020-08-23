74ac8b195d06c315f22eaaf12f034d44ab0fd41f
madhumithaa <madhumithaa1411@gmail.com>
2020-08-23 19:36:20 +0530

    react routing added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Navigation } from "react-router-dom";
 import LeftContainer from "./MajorComponents/LeftContainer";
 import RightContainer from "./MajorComponents/RightContainer";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="row">
-          <div className="col-lg-5">
-            <LeftContainer />
-          </div>
-          <div className="col-lg-7">
-            <RightContainer />
-          </div>
+      <div className="row">
+        <div className="col-lg-5">
+          <LeftContainer />
         </div>
-      </BrowserRouter>
+        <div className="col-lg-7">
+          <BrowserRouter>
+            <div>
+              {/* <Navigation /> */}
+              <Switch>
+                <Route path="/login" component={Login} exact />
+                <Route path="/signup" component={Signup} />
+                <Route component={Error} />
+              </Switch>
+            </div>
+          </BrowserRouter>
+        </div>
+      </div>
     );
   }
 }
